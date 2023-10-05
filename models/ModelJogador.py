401b8af2f376a9d798c5447f8dc32bec75ea8433
Layon Ribeiro <layonjroliveira@gmail.com>
2023-10-05 19:10:10 -0300

    Implementação MVC Jogador

---
+++ models/ModelJogador.py
@@ -0,0 +1,45 @@
+from models.ModelPartida import Partida
+from datetime import date as Date
+
+class Jogador:
+    def __init__(self, nome: str, nascimento: Date, senha: str, partida: Partida):
+        self.__nome = nome
+        self.__nascimento = nascimento
+        self.__senha = senha
+        self.__historico = []
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+    
+    @property
+    def nasciemnto(self):
+        return self.__nascimento
+    
+    @nasciemnto.setter
+    def nascimento(self, nascimento: Date):
+        self.__nascimento = nascimento
+    
+    @property
+    def senha(self):
+        return self.__senha
+    
+    @senha.setter
+    def senha(self, senha):
+        self.__senha = senha
+
+    @property
+    def ver_historico(self):
+        return self.__historico
+    
+    def incluir_partida(self, partida: Partida):
+    # assim quando a partida acaba deve ser incluida na lista de partidas do jogador
+        if isinstance(partida, Partida):
+            self.__historico.append(partida)
+    
+    # deve retornar a melhor partida do jogador (maior pontuação)
+    #def melhor_partida(self, partida: Partida)
---
+++ views/ViewJogador.py
@@ -0,0 +1,35 @@
+
+
+class ViewJogador:
+    def tela_opcoes(self):
+        print("------ OPÇÕES ------")
+        print("Escolha a opção")
+        print("1 - Jogar")
+        print("2 - Cadastrar Novo Jogador")
+        print("3 - Alterar Cadastro")
+        print("4 - Excluir Cadastro")
+        print("0 - Retonar")
+
+        opcao =  int(input("Escolha a opcao: "))
+        return opcao
+        
+    def pega_dados_jogador(self):
+        print("-------- INSIRA OS SEGUINTES DADOS ----------")
+        nome = input("Nome do Player: ")
+        nascimento = input("Nascimento do player (##/##/####): ")
+        senha = input("Senha: ")
+        return {"nome": nome, "nascimento" : nascimento, "senha": senha}
+    
+    def mostrar_jogador(self, dados_jogador):
+        print("NOME DO PLAYER: ", dados_jogador["nome"])
+        print("DATA DE NASCIMENTO: ", dados_jogador["nascimento"])
+        print("\n")
+    
+    def seleciona_jogador(self):
+        nome = input("Digite o no Player: ")
+        senha = input("Digite a senha: ")
+
+        return {"nome": nome, "senha": senha}
+    
+    def mensagem(self, mensagem: str):
+        print(mensagem)
